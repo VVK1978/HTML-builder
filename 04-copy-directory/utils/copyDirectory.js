c1b8635dd8d44d9e30393b7ac2a7a0ac0cfea742
VVK1978 <70840645+VVK1978@users.noreply.github.com>
2021-11-09 13:26:46 +0300

    fix: fix 04 solution

--- 04-copy-directory/utils/copyDirectory.js
+++ 04-copy-directory/utils/copyDirectory.js
@@ -2,33 +2,50 @@ const fs=require('fs');
 const path=require('path');
 
 function copyDir(sourceDir, targetDir) {
-  fs.rmdir(targetDir, { recursive: true },err=>{
-    if(err) throw err;
-    fs.mkdir(targetDir,{ recursive: true },err=>{
-      if(err) throw err;
-      fs.readdir(sourceDir,(err,files)=>{
-        if(err){
-          throw err;
-        }else {
-          files.forEach(file => {
-            fs.stat(path.join(sourceDir, file),(err,stats)=>{
-              if(err){
-                throw err;
-              } else
-              if(stats.isFile()){
-                fs
-                  .createReadStream(path.join(sourceDir, file))
-                  .pipe(fs.createWriteStream(path.join(targetDir, file)));
-              } else {
-                copyDir(path.join(sourceDir, file), path.join(targetDir, file));
-              }
-            });
-          }); 
+
+  fs.stat(targetDir, function(err) {
+    if (!err) {
+      fs.readdir(targetDir, (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+          fs.unlink(path.join(targetDir, file), err => {
+            if (err) throw err;
+          });
         }
+        copyFiles(sourceDir,targetDir);
       });
-    });
+    }
+    else if (err.code === 'ENOENT') {
+      fs.mkdir(targetDir,{ recursive: true, force:true },err=>{
+        if(err) throw err;
+        copyFiles(sourceDir,targetDir);
+      });
+    }
   });
+}
+
+function copyFiles(sourceDir,targetDir){
  
+  fs.readdir(sourceDir,(err,files)=>{
+    if(err){
+      throw err;
+    }else {
+      files.forEach(file => {
+        fs.stat(path.join(sourceDir, file),(err,stats)=>{
+          if(err){
+            throw err;
+          } else
+          if(stats.isFile()){
+            fs
+              .createReadStream(path.join(sourceDir, file))
+              .pipe(fs.createWriteStream(path.join(targetDir, file)));
+          } else {
+            copyDir(path.join(sourceDir, file), path.join(targetDir, file));
+          }
+        });
+      }); 
+    }
+  });
 }
 
 module.exports.copyDir=copyDir;
